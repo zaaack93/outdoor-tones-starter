de87dfbfd253ab861450ab2330895446c246b293
bouzangad zakaria <bouzangadzakaria@gmail.com>
2024-07-09 09:26:01 +0100

    working on stagger

---
+++ index.js
@@ -0,0 +1,64 @@
+class App{
+
+    constructor(){
+        this._initialise();
+        this._render();   
+    }
+
+
+    _initialise(){
+        this._setInitialStates();
+        this._createLines();
+        this._initIntro();
+        this._createHero();
+    }
+
+    _setInitialStates(){
+        gsap.set('.hero__title span,.text__effect p,.fullwidth-image__text',{
+            y:32,
+            opacity:0
+        })
+
+        gsap.set('.hero__images img',{
+            opacity:0,
+            y:gsap.utils.random(100,50)
+        })
+
+        gsap.set('.fullwidth-image img',{
+            scale:1.3
+        })
+    }
+
+    _initIntro(){
+        const t = gsap.timeline();
+
+        t.to('.hero__title div',{
+            opacity:1}).to('.hero__title span',{
+                opacity:1,
+                y:0,
+                duration:2,
+                ease:'expo.out',
+                stagger:0.01
+            }).to('.hero__images img',{
+                opacity:1,
+                y:0,
+                duration:2,
+                ease:'power3.out',
+                stagger:0.04
+            },0.5)
+    }
+
+    _createLines(){
+        this.lenis = new Lenis({
+            lerp:0.5,
+        })
+
+    }
+
+    _render(time){
+        this.lenis.raf(time)
+        requestAnimationFrame(this._render.bind(this))
+    }
+}
+
+new App();
